adbaf410263410b5b4636080ba0eb2cbf20a1afb
Luis Pimentel <lpimentel3@gatech.edu>
2020-09-19 02:20:33 -0400

    added dynamics linearization

--- CartPole/fnState_And_Control_Transition_Matrices.m
+++ CartPole/fnState_And_Control_Transition_Matrices.m
@@ -1,67 +1,60 @@
-function [A,B,C,c] = fnState_And_Control_Transition_Matrices(x,u,du,dt)
+function [A,B,C,c] = fnState_And_Control_Transition_Matrices(x,u)
 
-global d1;
-global d2;
-global d3;
-global b1;
-global b2;
-global b1_2;
-global b2_1;
+global g; 
+global m_c;
+global m_p; 
+global l; 
 
+
+% Linearize along nominal trajectory x
 x1 = x(1,1);
 x2 = x(2,1);
 x3 = x(3,1);
 x4 = x(4,1);
 
+% Linearize along nominal control  u
 u1 = u(1,1);
-u2 = u(2,1);
 
 
-det = d1 * d3  - d3^2 - (d2 * cos(x2))^2;
-
-
-
+%% Compute the Jacobian of Fx 
 A = zeros(4,4);
 
+A(1,1) = 0; 
+A(1,2) = 1; 
+A(1,3) = 0; 
+A(1,4) = 0;
 
-B = zeros(4,2);
+A(2,1) = 0; 
+A(2,2) = 0; 
 
-A(1,3) = 1;
-A(2,4)  = 1;
+a23_1 = ((m_p.*l.*(x4.^2).*cos(x3)).*(m_c + m_p.*(sin(x3).^2)) - (mp.*sin(x3).*l.*(x4.^2))*(2.*m_p.*sin(x3).*cos(x3)))./((m_c + m_p.*(sin(x3).^2)).^2); 
+a23_2 = ((-2.*g.*cos(x3).*sin(x3)).*(m_c + m_p.*(sin(x3).^2)) - (g.*(cos(x3).^2)).*(2*m_p.*sin(x3).*cos(x3)))./((m_c + m_p.*(sin(x3).^2)).^2); 
+a23_3 = (-u1.*2.*m_p.*sin(x3).*cos(x3))./((m_c + m_p.*(sin(x3).^2)).^2); 
+A(2,3) = a23_1 + a23_2 + a23_3; 
 
+A(2,4) = (2.*m_p.*sin(x3).*l.*x4)./((m_c + m_p.*(sin(x3).^2)).^2); 
 
-         
-A(3,2) = -2/(d1*d3-d3^2-cos(x2)^2)^2*(-d2*d3*(x3+x4)^2*sin(x2)-d2^2*x3^2*sin(x2)*cos(x2)-d2*(b2_1*x3+b2*x4)*cos(x2)+(d3*b1-d3*b2_1)*x3+(d3*b1_2-d3*b2)*x4)*cos(x2)*sin(x2)+1/(d1*d3-d3^2-cos(x2)^2)*(-d2*d3*(x3+x4)^2*cos(x2)-d2^2*x3^2*cos(x2)^2+d2^2*x3^2*sin(x2)^2+d2*(b2_1*x3+b2*x4)*sin(x2));
-A(3,3) = 1/(d1*d3-d3^2-cos(x2)^2)*(-2*d2*d3*(x3+x4)*sin(x2)-2*d2^2*x3*sin(x2)*cos(x2)-d2*b2_1*cos(x2)+d3*b1-d3*b2_1);
-A(3,4) = 1/(d1*d3-d3^2-cos(x2)^2)*(-2*d2*d3*(x3+x4)*sin(x2)-d2*b2*cos(x2)+d3*b1_2-d3*b2); 
-A(4,2) = -2/(d1*d3-d3^2-cos(x2)^2)^2*(d2*d3*x4*(2*x3+x4)*sin(x2)+d1*d2*x3^2*sin(x2)+d2^2*(x3+x4)^2*sin(x2)*cos(x2)+d2*((2*b2_1-b1)*x3+(2*b2-b1_2)*x4)*cos(x2)+(d1*b2_1-d3*b1)*x3+(d1*b2-d3*b1_2)*x4)*cos(x2)*sin(x2)+1/(d1*d3-d3^2-cos(x2)^2)*(d2*d3*x4*(2*x3+x4)*cos(x2)+d1*d2*x3^2*cos(x2)+d2^2*(x3+x4)^2*cos(x2)^2-d2^2*(x3+x4)^2*sin(x2)^2-d2*((2*b2_1-b1)*x3+(2*b2-b1_2)*x4)*sin(x2));
-A(4,3) = 1/(d1*d3-d3^2-cos(x2)^2)*(2*d2*d3*x4*sin(x2)+2*d1*d2*x3*sin(x2)+2*d2^2*(x3+x4)*sin(x2)*cos(x2)+d2*(2*b2_1-b1)*cos(x2)+d1*b2_1-d3*b1);
-A(4,4) = 1/(d1*d3-d3^2-cos(x2)^2)*(d2*d3*(2*x3+x4)*sin(x2)+d2*d3*x4*sin(x2)+2*d2^2*(x3+x4)*sin(x2)*cos(x2)+d2*(2*b2-b1_2)*cos(x2)+d1*b2-d3*b1_2);
- 
- 
+A(3,1) = 0; 
+A(3,2) = 0; 
+A(3,3) = 0; 
+A(3,4) = 1;
 
-B(3,1) = d3;
-B(3,2) = -(d3 + d2 * cos(x2));
-B(4,1) = B(3,2);
-B(4,2) = d1 + 2 * d2 * cos(x2);
+A(4,1) = 0; 
+A(4,2) = 0; 
 
-B = 1/det * B;
-c_1 = 1/det * [ 0 0 0 d3 * u1]';
-c_2 = 1/det * [0, 0, -d3 + d2 * cos(x2)*u1,d1 + 2 * d2 * cos(x2) * u2]';
+a43_1 = ((-m_p.*l.*(x4.^2).*(-sin(x3).*cos(x3) + (cos(x3).^2))).*(l.*(m_c + m_p.*(sin(x3).^2))) - (-m_p.*l.*(x4.^2).*cos(x3).*sin(x3)).*(2.*l.*m_p.*sin(x3).*cos(x3)))./((l.*(m_c + m_p.*(sin(x3).^2))).^2);
+a43_2 = ((-(m_c + m_p).*g.*cos(x3)).*(l.*(m_c + m_p.*(sin(x3).^2))) - (-(m_p + m_c).*g*sin(x3)).*(2.*l.*m_p.*sin(x3).*cos(x3)))./((l.*(m_c + m_p.*(sin(x3).^2))).^2);                             
+a43_3 = ((u1.*sin(x3)).*(l.*(m_c + m_p.*(sin(x3).^2))) - (-u1.*cos(x3)).*(2.*l.*m_p.*sin(x3).*cos(x3)))./((l.*(m_c + m_p.*(sin(x3).^2))).^2);
+A(4,3) = a43_1 + a43_2 + a43_3; 
 
-C_1 = zeros(4,2);
-C_1(3,1) = d3;
-C_1(4,2) = d2 * cos(x2);
-C_1 = C_1 * 1/det;
-
-C_2 = zeros(4,2);
-C_2(3,1) = d2 * cos(x2);
-C_2(4,2) = 2* d2 * cos(x2);
-C_2 = C_1 * 1/det;
-
-C = [C_1 C_2] *sqrt(dt);
-c = [c_1 c_2]*sqrt(dt);
+A(4,4) = (-2.*m_p.*l.*cos(x3).sin(x3).*x4)./((l.*(m_c + m_p.*(sin(x3).^2))).^2);
 
 
+%% Compute the Jacobian of Fu 
 
+B = zeros(4,4);
 
+B(1,1) = 0;
+B(2,1) = (1)./(m_c + m_p.*(sin(x3).^2))
+B(3,1) = 0;
+B(4,1) = (-cos(x3))./(l.*(m_c + m_p.*(sin(x3).^2))); 
--- Inverted Pendulum/fnState_And_Control_Transition_Matrices.m
+++ Inverted Pendulum/fnState_And_Control_Transition_Matrices.m
@@ -1,32 +1,24 @@
-function [A,B,C,c] = fnState_And_Control_Transition_Matrices(x,u,du,dt)
+function [A,B] = fnState_And_Control_Transition_Matrices(x,u)
 
-global m_c;
-global m_p;
 global g;
-global l;
+global m; 
+global l; 
+global I; 
+global b;
 
 x1 = x(1,1);
 x2 = x(2,1);
-x3 = x(3,1);
-x4 = x(4,1);
 
 u1 = u(1,1);
-u2 = u(2,1);
 
-A = zeros(4,4);
+A = zeros(2,2);
+
+A(1,2) = 0; 
 A(1,2) = 1;
-A(2,3) = -(m_p*g)/m_c;
-A(3,4) = 1;
-A(4,3) = -((m_c + m_p)*g)/(l*m_c);
+A(2,1) = -((m*g)/I)*cos(x1);
+A(2,2) = -b/I;
 
-B = zeros(4,1);
-B(2,1) = 1/m_c;
-B(4,1) = 1/(l*m_c);
-
-C = zeros(2,4);
-C(1,1) = 1;
-C(2,3) = 1;
-
-
-
+B = zeros(2,1);
+B(1,1) = 0;
+B(2,1) = 1; 
 
