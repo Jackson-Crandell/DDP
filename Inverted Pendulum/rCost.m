9f485117912e15cb1a8eb2ab57de5bcda83bd34a
Luis Pimentel <lpimentel3@gatech.edu>
2020-09-29 18:54:40 -0400

    changes to cost

--- Inverted Pendulum/rCost.m
+++ Inverted Pendulum/rCost.m
@@ -1,8 +1,17 @@
-function  [l0,l_x,l_xx,l_u,l_uu,l_ux] = rCost(x, u, k,R,dt)
+function  [l0,l_x,l_xx,l_u,l_uu,l_ux] = rCost(x, u, k,R,P,target, dt)
 
-l0 = u' * R * u;
-l_x =zeros(2,1);
-l_xx = zeros(2,2);
+l0 = 0.5*(u'*R*u + (x - target)'*P*(x - target));
+l_x = P*(x - target);
+l_xx = P; 
+
 l_u = R * u;
 l_uu = R;
 l_ux = zeros(1,2);
+
+
+% l0 = u'*R*u;
+% l_x = zeros(2,1);
+% l_xx = zeros(2,2); 
+% l_u = R * u;
+% l_uu = R;
+% l_ux = zeros(1,2);
