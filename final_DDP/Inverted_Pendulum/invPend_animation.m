5cced5da9e7f403057fa9d1f06dfa97fac95e9c7
Jackson-Crandell <jackson.crandell@mac.com>
2020-09-30 21:46:46 -0400

    Added Saving Animation option

--- final_DDP/Inverted_Pendulum/invPend_animation.m
+++ final_DDP/Inverted_Pendulum/invPend_animation.m
@@ -7,13 +7,22 @@ for i=1:horizon
     P=1*[sin(x(1,i)) -cos(x(1,i))];
     
     pend=line([O(1) P(1)],[O(2) P(2)], 'LineWidth', 4);
-    
+    F(i) = getframe(gcf);
     pause(0.01);
     if i<1000
         
         delete(pend);
         
     end
-    end
+end
+
+save_video = false;
+if (save_video)
+    video = VideoWriter('InvPend.avi','Uncompressed AVI');
+    open(video)
+    writeVideo(video,F)
+    close(video)
+end
 
 end
+
